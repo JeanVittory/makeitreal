5fa8b69cf5f18c2babc0ba1ca4887dbf79718ad5
JeanVittory <vittory.dev@gmail.com>
2023-02-02 17:52:42 -0500

    init

---
+++ dia4/mañana/README.md
@@ -0,0 +1,50 @@
+# Make It Real - NAME OF THE PROJECT
+
+This is a solution to the **\_\_\_** project of the Make It Real course.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the design at 320px to 375px in mobile
+- See the design at 376px to 1440px for desktop
+
+### Screenshot
+
+#### Desktop
+
+![](./screenshots/desktop.png)
+
+#### Mobile
+
+![](./screenshots/mobile.png)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+### What I learned
+
+We learned how to implement clamps() in fonts to make them more responsiveness. The layout improve
+our ability with flex and grid
+
+## Author
+
+Jean Vittory, Michael Saenz & Camilo Gil
---
+++ dia4/tarde/index.js
@@ -0,0 +1,60 @@
+// 1. Devolver propiedad de objeto anidado
+// Escribir una función que tome un objeto como argumento. En algunos casos, el objeto contiene otros objetos con algunas propiedades profundamente anidadas.
+// Devuelve el valor de la propiedad 'b' del objeto 'a' dentro del objeto original si existe, si no, devuelve undefined.
+
+console.log('********* ejercicio 1 *********');
+
+const searchingValueObj = (obj) => {
+	if (!obj.a) return undefined;
+	if (!obj.a?.b) return undefined;
+
+	return obj.a.b;
+};
+
+console.log(searchingValueObj({ a: 1 })); // undefined
+console.log(searchingValueObj({ a: { b: { c: 3 } } })); // 	{c:3}
+console.log(searchingValueObj({ b: { a: 1 } })); // undefined
+console.log(searchingValueObj({ a: { b: 2 } })); // 2
+
+// 2. Fix Error: manipulación de arreglos
+// ¡Ayuda a corregir todos los errores en la función incrementItems! ¡Está destinado a agregar 1 a cada elemento en el arreglo!
+
+function incrementItems(arr) {
+	let arrIncrement = [];
+	for (let i = 0; i < arr.length; i++) {
+		arrIncrement.push(arr[i] + 1);
+	}
+	return arrIncrement;
+}
+
+console.log('********* ejercicio 2 *********');
+
+console.log(incrementItems([0, 1, 2, 3])); //[1, 2, 3, 4]
+console.log(incrementItems([2, 4, 6, 8])); // [3, 5, 7, 9]
+console.log(incrementItems([-1, -2, -3, -4])); // [0, -1, -2, -3]
+
+// 3. Fix Error: Valor vs. Referencia de Tipos
+// Cree una función que devuelva true si dos arreglos contienen valores idénticos y false en caso contrario.
+
+// Para resolver esta pregunta, tu amigo escribe el siguiente código:
+
+function checkEquals(arr1, arr2) {
+	let newArr = [];
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) {
+			newArr.push(false);
+		} else {
+			newArr.push(true);
+		}
+	}
+
+	return !newArr.includes(false);
+}
+
+console.log('********* ejercicio 3 *********');
+
+console.log(checkEquals([1, 2], [1, 3])); //false
+console.log(checkEquals([1, 2], [1, 2])); // true
+console.log(checkEquals([4, 5, 6], [4, 5, 6])); //true
+console.log(checkEquals([4, 7, 6], [4, 5, 6])); // false
+console.log(checkEquals([4, 7, 6], [4, 6, 7])); // false
