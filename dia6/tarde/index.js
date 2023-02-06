db80c748bce394da01808e698c157d19feadebcf
JeanVittory <vittory.dev@gmail.com>
2023-02-06 16:44:33 -0500

    refactoring sumObj

--- dia6/tarde/index.js
+++ dia6/tarde/index.js
@@ -2,12 +2,11 @@
 // Escribir una función que tome un objeto como argumento. Devuelve la suma de todos los valores de las propiedades del objeto.
 
 const sumValues = (obj) => {
-	return Object.values(obj).reduce((initial, value) => {
-		value += initial;
-		return value;
-	}, 0);
+	if (!(typeof obj === 'object')) return 'the param should be an object';
+	return Object.values(obj).reduce((initial, value) => (value += initial), 0);
 };
 
+console.log(sumValues(1));
 console.log(sumValues({ a: 1, b: 2, c: 3 })); //6
 console.log(sumValues({ j: 9, i: 2, x: 3, z: 4 })); // 18
 console.log(sumValues({ w: 15, x: 22, y: 13 })); //50
@@ -19,10 +18,12 @@ console.log(sumValues({ w: 15, x: 22, y: 13 })); //50
 // Escribe una función recurrente que calcule el número n de la secuencia (basado en 0). Por ejemplo:
 
 const fib = (num) => {
-	// break
 	if (num === 0 || num === 1) return 1;
-	//base
 	return fib(num - 1) + fib(num - 2);
 };
 
-console.log(fib(10));
+console.log('---------fibonacci----------');
+console.log(fib(0));
+console.log(fib(1));
+console.log(fib(4));
+console.log(fib(5));
