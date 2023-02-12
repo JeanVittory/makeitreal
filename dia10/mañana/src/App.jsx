f4dd38c0f34b276ba938435b7e505b805b7bf2ff
JeanVittory <vittory.dev@gmail.com>
2023-02-12 18:24:25 -0500

    fixing background

--- dia10/mañana/src/App.jsx
+++ dia10/mañana/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import { Card } from './components/Card/Card';
+import bgDesktop from './assets/pattern-background-desktop.svg';
+import bgMobile from './assets/pattern-background-mobile.svg';
 
 function App() {
 	return (
